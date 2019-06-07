23e90b1e1b808c11d831d6cc5c1037a1f9d14bf6
KIRTAN PATEL <kirtanpatel24399@gmail.com>
2019-06-07 06:42:02 -0400

    Updated Logic for verifyTranscript

--- logic.js
+++ logic.js
@@ -37,35 +37,40 @@ function sign(signature){
  * @transaction
  */
  function verify(verifyTranscript){
-     var transcriptHash=verifyTranscript.transcriptHash;
-     var batchID=verifyTranscript.batchID;
-     console.log(batchID);
-     var merklePath=verifyTranscript.merklePath;
-     var user=verifyTranscript.user;
-     var result=verifyTranscript.result;
-     var universityIDs=verifyTranscript.university.universityID;
-     // Get the vehicle asset registry.
+     //Get All the objects into local variable.
+     var t_transcriptHash=verifyTranscript.transcriptHash;
+     var t_batchID=verifyTranscript.batchID;
+     var t_merklePath=verifyTranscript.merklePath;
+     var t_user=verifyTranscript.user;
+     var t_university=verifyTranscript.university;
+     // Get the MerkleRoot Asset Registry
+   console.log(t_batchID);
     return getAssetRegistry('org.daiict.mynetwork.MerkleRoot')
         .then(function (assetRegistry) {
-        // Get the specific vehicle from the vehicle asset registry.
-            return assetRegistry.get(batchID);
+        // Get the specific merkle Root from the Merkle Root asset registry.
+            return assetRegistry.get(t_batchID);
         })
         .then(function (merkleRoot){
-        console.log(merkleRoot.merkleRootValue);
-        console.log(merkleRoot.university);
-        // Process the the vehicle object.
-        var merkleRootValue=merkleRoot.merkleRootValue;
-        var universityIDstored=merkleRoot.university;
-        if(universityIDstored.universityID != universityIDs){
-            // Get the vehicle asset registry.
-            console.log('Its not a match');
-            result='universityID not valid';
-            // The existing vehicle that has come from elsewhere.
-            // Get the vehicle asset registry.
-            return getAssetRegistry('org.daiict.mynetwork.MerkleRoot')
-            .then(function (merkleRootAssetRegistry) {
-                // Update the vehicle in the vehicle asset registry.
-                return merkleRootAssetRegistry.update(batchID);
+        var d_university=merkleRoot.university;
+        var d_merkleRootValue=merkleRoot.merkleRootValue;
+        var d_status=merkleRoot.status;
+        console.log(d_university);
+        console.log(d_merkleRootValue);
+        console.log(t_university.universityID);
+        //Compare UniversityID's
+        if(t_university.universityID != d_university.universityID){
+            var factory=getFactory();
+            var random=Math.floor((Math.random() * 1000) + 1);
+            var verificationID=t_user.userID + random;
+            var new_verification=factory.newResource('org.daiict.mynetwork','verificationRequest',verificationID);
+            new_verification.result='universityID not valid';
+            new_verification.user=t_user;
+            new_verification.transcriptHash=t_transcriptHash;
+            // Get the verificationRequests Asset Registry.
+            return getAssetRegistry('org.daiict.mynetwork.verificationRequest')
+            .then(function (verificationAssetRegistry) {
+                // Add the new verificationRequest to the AssetRegistry in the vehicle asset registry.
+                return verificationAssetRegistry.add(new_verification);
             })
             .catch(function (error1) {
                 // Add optional error handling here.
@@ -73,36 +78,44 @@ function sign(signature){
             });
         }
         else{
-      		console.log(merkleRoot.mmerkleRootValue);
-            console.log(transcriptHash);
-            if(merkleRoot.merkleRootValue==transcriptHash){  
-                console.log('Its a match');
-                result='Verified';
-                // The existing vehicle that has come from elsewhere.
-                // Get the vehicle asset registry.
-                return getAssetRegistry('org.daiict.mynetwork.MerkleRoot')
-                .then(function (merkleRootAssetRegistry) {
-                    // Update the vehicle in the vehicle asset registry.
-                    return merkleRootAssetRegistry.update(batchID);
+            console.log(d_merkleRootValue);
+           console.log(t_transcriptHash);
+            if(d_merkleRootValue==t_transcriptHash){  
+                var factory=getFactory();
+                var random=Math.floor((Math.random() * 1000) + 1);
+                var verificationID=t_user.userID + random;
+                var new_verification=factory.newResource('org.daiict.mynetwork','verificationRequest',verificationID);
+                new_verification.result='Valid Transcript';
+                new_verification.user=t_user;
+                new_verification.transcriptHash=t_transcriptHash;
+                // Get the verificationRequests Asset Registry.
+                return getAssetRegistry('org.daiict.mynetwork.verificationRequest')
+                .then(function (verificationAssetRegistry) {
+                    // Add the new verificationRequest to the AssetRegistry in the vehicle asset registry.
+                    return verificationAssetRegistry.add(new_verification);
                 })
-                .catch(function (error2) {
+                .catch(function (error1) {
                     // Add optional error handling here.
-                    throw new Error('Unable to update positive result');
+                    throw new Error('Problem retreiving the universityID');
                 });
-
             }
             else{
-                result='Tampered Transcript';
-                // The existing vehicle that has come from elsewhere.
-                // Get the vehicle asset registry.
-                return getAssetRegistry('org.daiict.mynetwork.MerkleRoot')
-                .then(function (merkleRootAssetRegistry) {
-                    // Update the vehicle in the vehicle asset registry.
-                    return merkleRootAssetRegistry.update(batchID);
+                var factory=getFactory();
+                var random=Math.floor((Math.random() * 1000) + 1);
+                var verificationID=t_user.userID + random;
+                var new_verification=factory.newResource('org.daiict.mynetwork','verificationRequest',verificationID);
+                new_verification.result='Tampered Transcript';
+                new_verification.user=t_user;
+                new_verification.transcriptHash=t_transcriptHash;
+                // Get the verificationRequests Asset Registry.
+                return getAssetRegistry('org.daiict.mynetwork.verificationRequest')
+                .then(function (verificationAssetRegistry) {
+                    // Add the new verificationRequest to the AssetRegistry in the vehicle asset registry.
+                    return verificationAssetRegistry.add(new_verification);
                 })
-                .catch(function (error3) {
+                .catch(function (error1) {
                     // Add optional error handling here.
-                    throw new Error('Unable to update negative result');
+                    throw new Error('Problem retreiving the universityID');
                 });
             }
         }
